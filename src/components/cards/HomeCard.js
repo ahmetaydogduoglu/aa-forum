1033f957f284087ba98dd93e4f861c7ff37dd88c
Ahmet Aydoğduoğlu <47844516+ahmetaydogduoglu@users.noreply.github.com>
2020-06-04 01:51:09 +0300

    card desing started

---
+++ src/components/cards/HomeCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./HomeCard.css"
+export default function HomeCard() {
+    return (
+        <div className="card-container">
+            <div style={{width:"35%",height:"100%",backgroundSize:"cover",backgroundImage:"url(https://gezimanya.com/sites/default/files/styles/800x600_/public/gezi-notu/2019-11/image-amsterdam_2.jpg)"}}>
+       
+            </div>
+        </div>
+    )
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,11 +2,16 @@ import React from 'react'
 import "./Home.css"
 import Navbar from "../components/Navbar"
 import CategoriesList from "../components/categories/CategoriesList"
+import HomeCard from '../components/cards/HomeCard'
 export default function Home() {
     return (
         <div className="container">
             <Navbar />
             <CategoriesList />
+            <div className="card-list-container">
+                <HomeCard/>
+                <HomeCard/>
+            </div>
         </div>
     )
 }
